3bcfa81878b18e2b6d2c28a7713c6b2c075ab39a
Boris Horvat <horvat.boris99@gmail.com>
2021-08-22 01:24:53 +0200

    Odradjeno logovanje samo sa serijalizovanim korisnicima

---
+++ static/views/Signin.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="sign-in-main">
+    <div id="sign-in-container">
+      <form @submit="signin" id="sign-in-form">
+        <input v-model="username" type="text" placeholder="Username">
+        <input v-model="password" type="text" placeholder="Password">
+        <input class="button-primary" type="submit" value="Sign In">
+      </form>
+
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data() {
+    return{
+    username: '',
+        password: ''
+      }
+    },
+  methods:{
+    signin: function (e){
+      e.preventDefault();
+      let data = {
+        username: this.username,
+        password: this.password,
+      };
+
+      axios.post('/user/login', data).then(r => {
+        localStorage.jws = r.data;
+        this.$router.push('/');
+      })
+    }
+  }
+};
+</script>
