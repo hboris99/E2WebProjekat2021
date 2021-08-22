a3802fc2280235f6d3f941aef19d9ba10783a38e
Boris Horvat <horvat.boris99@gmail.com>
2021-08-22 02:26:11 +0200

    Odradjena provera ne unosenog passworda

--- static/views/Signin.vue
+++ static/views/Signin.vue
@@ -5,8 +5,13 @@
       <p>Enter your credentials</p>
       <form @submit="signin" id="sign-in-form">
         <label for="form-name-input">Username:</label>
+        <b class="error">{{errors.signin}}</b>
+        <b class="error">{{errors.username}}</b>
+
         <input id="form-name-input" v-model="username" type="text" placeholder="Enter your username...">
         <label for="form-pass-input">Password:</label>
+        <b class="error">{{errors.password}}</b>
+
         <input id="form-pass-input" v-model="password" :type="type" id="password" placeholder="Enter your password" autocomplete="off">
         <button type="button" @click="showPassword()">{{btnText}}</button>
         <input class="button-primary" type="submit" value="Sign In">
@@ -26,7 +31,12 @@ module.exports = {
     username: '',
         password: '',
       type:'password',
-      btnText: 'Show'
+      btnText: 'Show',
+      errors: {
+        username: '',
+        password: '',
+        signin: '',
+      },
       }
     },
   methods:{
@@ -39,8 +49,20 @@ module.exports = {
         this.btnText='Show';
       }
     },
+    validateInputs: function (){
+      this.errors.signin ='';
+      let usernameValid = !!this.username;
+      let passwordValid = !!this.password;
+      this.errors.username = usernameValid ? '' : 'Username required';
+      this.errors.password = passwordValid ? '' : 'Password required';
+      return usernameValid && passwordValid;
+    },
     signin: function (e){
       e.preventDefault();
+
+      if(!this.validateInputs()) {
+        return;
+      }
       let data = {
         username: this.username,
         password: this.password,
